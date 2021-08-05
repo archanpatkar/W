205255a0193ff4e4cc27d848110c78bc05457331
Archan Patkar <pat@archanpatkar.com>
2021-08-05 17:02:23 +0530

    updates

--- __test__/tokenizer.test.js
+++ __test__/tokenizer.test.js
@@ -1,9 +1,54 @@
-const sum = require('../src/tokenizer');
+const  Tokenizer = require('../src/tokenizer');
+
 
 // Basic Init
-test('adds 1 + 2 to equal 3', () => {
-  expect(1+2).toBe(3);
+test('Double dot checking', () => {
+  expect(() => new Tokenizer(`10.3.4`).next())
+      .toThrowError(/multiple dots/); 
+  expect(() => console.log(new Tokenizer(`1..4`).next())).toThrowError(/multiple dots/);
 });
+
+// Testing
+// 
+
+// try {
+//     t1.next();
+// } catch(e) {
+//     console.log(e.message);
+// }
+
+// try {
+//     t1.reset("'a");
+//     t1.next();
+// } catch(e) {
+//     console.log(e.message);
+// }
+
+// try {
+//     t1.reset('"this is a test');
+//     t1.next();
+// } catch(e) {
+//     console.log(e.message);
+// }
+
+// try {
+//     t1.reset(`
+//         "this is a test"
+//         @
+//     `);
+//     console.log(t1.next().toString());
+//     console.log(t1.next());
+//     console.log(t1.next());
+//     console.log(t1.next());
+//     console.log(t1.next());
+//     console.log(t1.next());
+//     console.log(t1.next());
+// } catch(e) {
+//     console.log("");
+//     console.log(e.message);
+// }
+
+
 
 // const t1 = new Tokenizer(
 // `       10      20 22   2223  40  
